911d3d6d4f8d94cd8c9de82c46b287b53ddb5212
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-08-15 10:44:50 +0200

    listened to port 3000 and tested it

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+const homeStartingContent = "This is some dummy Home content";
+const aboutContent = "This is some dummy About content";
+const contactContent = "This is some dummy Contact content";
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
