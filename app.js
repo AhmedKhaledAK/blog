e6df63f78159161ecd3b63d4d78ff159501aa844
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-08-18 15:42:15 +0200

    created posts array and pushing each post in it whenever a post request is received to /compose

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const homeStartingContent = "This is some dummy Home content";
 const aboutContent = "This is some dummy About content";
 const contactContent = "This is some dummy Contact content";
 
+let posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,6 +19,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("home", {homeContent: homeStartingContent});
+  console.log(posts);
 });
 
 app.get("/home", function(req, res){
@@ -42,8 +45,15 @@ app.post("/compose", function(req, res){
     body: req.body.postContent
   };
 
-  console.log("post title: " + post.title);
-  console.log("post content: " + post.body);
+  //console.log("post title: " + post.title);
+  //console.log("post content: " + post.body);
+
+  if(req.body.postTitle && req.body.postContent){
+    posts.push(post);
+  } else {
+    res.send("Post not submitted! Missing the title or the body.");
+  }
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
