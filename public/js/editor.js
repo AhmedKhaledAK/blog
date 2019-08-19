ddcbc7322f8db413b04bf1ac19d7e4e88038fbb9
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-08-19 23:30:00 +0200

    previewed the post content correctly in home page and post page

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,12 +1,14 @@
 window.onload = function() {
     var converter = new showdown.Converter();
-    var pad = document.getElementById('pad');
-    var markdownArea = document.getElementById('markdown');
+    var pad = document.getElementById("editor");
+    var markdownArea = document.getElementById("markdown");
+    var hiddenTextArea = document.getElementById("hiddenTextArea");
 
     var convertTextAreaToMarkdown = function(){
         var markdownText = pad.value;
         html = converter.makeHtml(markdownText);
         markdownArea.innerHTML = html;
+        hiddenTextArea.innerHTML = html;
     };
 
     pad.addEventListener('input', convertTextAreaToMarkdown);
